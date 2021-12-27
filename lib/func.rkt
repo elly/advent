8bb6a69bd1a82fc61bd16eab0d5f4bd309cd6a5d
Elly <elly@elly.town>
2021-12-27 10:34:22 -0800

    lib/func: document

--- lib/func.rkt
+++ lib/func.rkt
@@ -2,6 +2,10 @@
 
 (provide iterate-n iterate-until)
 
+; Given a function, a count, and a value, repeatedly apply that function
+; to that value that many times. For example:
+;   (iterate-n sub1 3 10)
+; would be 7.
 (define/contract (iterate-n f n x)
   (-> procedure? integer? any/c any/c)
   (let loop ((n n) (x x))
@@ -9,6 +13,8 @@
         x
         (loop (sub1 n) (f x)))))
 
+; Given a function, a predicate, and a value, iterate that function until the
+; predicate returns true on the old value and the new value.
 (define/contract (iterate-until f p x)
   (-> procedure? procedure? any/c any/c)
   (let loop ((x x))
@@ -16,4 +22,3 @@
       (if (p x nx)
           x
           (loop nx)))))
-        
