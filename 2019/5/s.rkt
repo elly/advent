a0a1a4739e22cf8a999ef04240ffabfb4c0364c7
Elly <elly@elly.town>
2021-12-30 09:48:12 -0800

    2019/5: neaten

--- 2019/5/s.rkt
+++ 2019/5/s.rkt
@@ -3,13 +3,7 @@
 (provide today)
 (require "../intcode.rkt")
 
-(define (solve-a vm)
-  (with-fixed-io vm '(1) last))
-
-(define (solve-b vm)
-  (with-fixed-io vm '(5) last))
-
 (define today (list (curryr icvm-load 8192)
                     identity
-                    solve-a
-                    solve-b))
+                    (curryr with-fixed-io '(1) last)
+                    (curryr with-fixed-io '(5) last)))
