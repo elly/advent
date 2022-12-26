2784a4f0a5ca26cd4ff7534c886be963b4bfd37a
Elly Fong-Jones <elly@elly.town>
2022-12-26 07:02:46 -0800

    2019: remove old racket solutions

--- 2019/5/s.rkt
+++
@@ -1,9 +0,0 @@
-#lang racket
-
-(provide today)
-(require "../intcode.rkt")
-
-(define today (list (curryr icvm-load 8192)
-                    identity
-                    (compose (curryr with-fixed-io '(1) last) icvm-copy)
-                    (compose (curryr with-fixed-io '(5) last) icvm-copy)))
