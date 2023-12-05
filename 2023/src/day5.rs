9bd5d87a47ed98c5d7170cdaf586046093498a92
Elly Fong-Jones <elly@elly.town>
2023-12-04 21:41:06 -0800

    2023/5: solve

---
+++ 2023/src/day5.rs
@@ -0,0 +1,77 @@
+// Day 5: If You Give A Seed A Fertilizer
+
+#[derive(Debug)]
+struct Mapping {
+    dest: u64,
+    src: u64,
+    len: u64,
+}
+
+#[derive(Debug)]
+struct Input {
+    seeds: Vec<u64>,
+    maps: Vec<Vec<Mapping>>,
+}
+
+fn parse(input: &str) -> Input {
+    let mut lines = input.split('\n');
+    let seeds: Vec<_> = lines.next().unwrap().split(' ')
+                               .skip(1).map(|x| x.parse::<u64>().unwrap())
+                               .collect();
+    let mut maps: Vec<Vec<Mapping>> = Vec::new();
+    while let Some(line) = lines.next() {
+        if line.is_empty() {
+            maps.push(Vec::<Mapping>::new());
+        } else if line.chars().next().unwrap().is_digit(10) {
+            let parts: Vec<_> =
+                line.split(' ').map(|x| x.parse::<u64>().unwrap()).collect();
+            maps.last_mut().unwrap().push(Mapping { 
+                dest: parts[0], src: parts[1], len: parts[2]
+            });
+        }
+    }
+    Input { seeds, maps }
+}
+
+fn convert_one(map: &Vec<Mapping>, seed: u64) -> u64 {
+    for m in map {
+        if seed >= m.src && seed < m.src + m.len {
+            return m.dest + (seed - m.src);
+        }
+    }
+    seed
+}
+
+fn convert(maps: &Vec<Vec<Mapping>>, seed: u64) -> u64 {
+    maps.iter().fold(seed, |s, m| { convert_one(m, s) })
+}
+
+fn parta(input: &Input) -> u64 {
+    let mut r: Vec<_> = input.seeds.iter().map(|s| convert(&input.maps, *s)).collect();
+    r.sort();
+    r[0]
+}
+
+// This is really really slow - it takes a few minutes to run on my solution.
+// There's probably a more clever way to do this. #450 on global LB though so
+// who can say whether it is good or bad
+fn partb(input: &Input) -> u64 {
+    let mut lowest: u64 = 9999999999;
+    for r in input.seeds.chunks(2) {
+        let start = r[0];
+        let len = r[1];
+        dbg!((start, len));
+        for x in start .. start + len {
+            let c = convert(&input.maps, x);
+            if c < lowest {
+                lowest = dbg!(c);
+            }
+        }
+    }
+    lowest
+}
+
+pub fn solve(input: &str) -> (String, String) {
+    let input = parse(input);
+    (parta(&input).to_string(), partb(&input).to_string())
+}
--- 2023/src/main.rs
+++ 2023/src/main.rs
@@ -6,13 +6,14 @@ mod day1;
 mod day2;
 mod day3;
 mod day4;
+mod day5;
 
 type SolveFn = fn(&str) -> (String, String);
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     let days: Vec<SolveFn> = vec![day0::solve, day1::solve, day2::solve,
-                                  day3::solve, day4::solve];
+                                  day3::solve, day4::solve, day5::solve];
 
     if args.len() < 2 {
         panic!("Usage: <day number>");
