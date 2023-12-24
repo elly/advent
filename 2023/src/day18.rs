dd3208a7e982731cc2f08dbbd7302526e10e7248
Elly Fong-Jones <elly@elly.town>
2023-12-23 21:10:26 -0800

    2023/18b: solve :D

--- 2023/src/day18.rs
+++ 2023/src/day18.rs
@@ -1,126 +1,17 @@
 // Day 18: Lavaduct Lagoon
 //
-// Thinking notes: part B is going to require a fully different approach; the
-// lagoon is too big to construct the actual map. We can store the set of
-// corners, though, since there's only one corner per instruction. Maybe we
-// could then decompose the contained shape into a bunch of separate rectangles
-// and add up their sizes? Hmmm.
-//
-// How would we find a rectangle? Every corner has to be the corner of a
-// rectangle, since the whole diagram is a single loop... time to ponder.
-//
-// Here's an idea: the resulting shape is the union of a collection of
-// rectangles, and if we could figure out where those rectangles are, we could
-// compute the total area they should occupy. To do that, we can identify
-// outermost edges and find the edges connected to them; once we have three
-// connected edges, we can use them to define a rectangle, remove the three
-// edges, and splice a new edge in where the rectangle was. Here's an example:
-//
-//          #######
-//          #.....#
-//   ########.....#
-//   #............#
-//   #............221
-//   ###............1
-//     #............1
-//     #............1
-//     #####...###221
-//         #...#
-//         #####
-//
-// We can pick an outermost edge (marked with 1s) then walk along both its
-// perpendicular edges until one of them turns. There is now a rectangle defined
-// by those three edges, and we can remove it and add its fourth edge to the
-// shape, producing this:
-//
-//          1111111
-//          2.....2
-//   #######2.....2
-//   #............#
-//   #............#
-//   ###..........#
-//     #..........#
-//     #..........#
-//     #####...####
-//         #...#
-//         #####
-// And repeat:
-//   ##############
-//   #............#
-//   #............#
-//   ###..........#
-//     #..........#
-//     #..........#
-//     #####...####
-//         #...#
-//         #####
-// And repeat continuously, summing the sizes of the rectangles we removed,
-// until we run out of rectangles. That seems like it should work.
-//
-// How do we implement that? It seems like we can keep a list of all the edges
-// in the order they appear (which will be easy to produce from the input), then
-// gradually remove stuff from that list. We won't actually need many cases, on
-// further reflection, since we can always just take the topmost edge and go
-// from there...? Does that always work?
-//
-// What about this though:
-//
-// 1######2
-// #......#
-// 0#####.*
-//      #.#
-//      #.3###
-// for that we need a new point at *. For this:
-//
-// 1########2
-// #........3##
-// 0#######.*..
-//        #....
-// The big problem is, how do we ensure we're accounting for all the space? For
-// a case like the above one, we have to include all the edges (so we don't
-// forget them) but not double-count any tiles. It seems like to get that to
-// work we'd have to subtract out the area we're leaving behind from the area of
-// the rectangle - but that seems like it'd require some case analysis on how
-// the rectangle is attached to the larger shape? Yuck.
-//
-// That whole approach seems like it won't work. Rats.
-//
-// New approach idea: what if we scanned down the shape line by line? The y
-// coordinates probably range across like, 0 - 2**24 or so (say), so what if for
-// each y coordinate, we had a notion of which ranges lie inside the shape? The
-// left edge of the range is always outside the shape, and crossing a vertical
-// line puts you inside it - but what about horizontal lines? are you inside or
-// outside afterwards?
-//
-// We'd also need to keep track of ranges that are inside the shape as we go
-// row-by-row, and you're only inside if you are inside for both. So for
-// example, let's say we have this shape:
-//
-//   ####      
-//   #  #      ###
-//   #  #      # #
-//   ## ######## #
-//    #    ##    #
-//    ###     ####
-//      #######
-// I think it's not that straightforward. Hm. When we're walking down the walls,
-// we lose track of whether we're inside or outside. :(
-//
-// I went and did some reading and learned about the Shoelace Formula:
-// https://en.wikipedia.org/wiki/Shoelace_formula - maybe I can use this, even
-// though it's a bit too general. Maybe this will work? I'm worried about how to
-// handle the thickness of the lines though - to have a line that encompasses
-// the shape, we'd have to outset *some* of the coordinates - the ones that are
-// like, on the "inside" of the shape - to account for the extra space on those
-// edges.
-//
-// I'm super stuck on this problem. I'm gonna put it aside for now and come back
-// to it.
-use crate::map2d::{Dir2d};
+// Here's the plan: we find the line that encloses the lagoon, then use the
+// Shoelace Method to find the area of that polygon. To find the line that
+// encloses the lagoon, we walk along the trench which defines its perimeter; we
+// need to use the outside edge of that perimeter trench. We don't have an easy
+// way to tell which edge is the outside one, so we try walking both edges and
+// look for the longest perimeter.
+use crate::map2d::{Dir2d,Point2d};
+use geo::Area;
+use geo::geometry::{Coord,LineString,Polygon};
 
 type Step = (Dir2d, i32);
 
-#[allow(dead_code)]
 fn parse_step(input: &str, flip: bool) -> Step {
     let parts: Vec<_> = input.split(' ').collect();
     if flip {
@@ -149,8 +40,91 @@ fn parse_step(input: &str, flip: bool) -> Step {
     }
 }
 
-pub fn solve(_input: &str) -> (String, String) {
-    ("".to_string(), "".to_string())
+fn parse(input: &str, flip: bool) -> Vec<Step> {
+    input.split('\n').filter(|x| !x.is_empty())
+                     .map(|x| parse_step(x, flip))
+                     .collect()
+}
+
+// We're given the side step entering a corner and the step leaving it; figure
+// out whether we need to move this corner outwards in each coordinate or not.
+fn corner(p: &Point2d, s0: &Step, s1: &Step, sideb: bool) -> Coord<f64> {
+    type C = Coord<f64>;
+    let fx = p.x as f64;
+    let fy = p.y as f64;
+    if sideb {
+        match (s0.0, s1.0) {
+            (Dir2d::East, Dir2d::South) => C { x: fx + 1., y: fy },
+            (Dir2d::East, Dir2d::North) => C { x: fx + 1., y: fy + 1. },
+            (Dir2d::West, Dir2d::South) => C { x: fx,      y: fy },
+            (Dir2d::West, Dir2d::North) => C { x: fx,      y: fy + 1. },
+
+            (Dir2d::North, Dir2d::East) => C { x: fx + 1., y: fy + 1. },
+            (Dir2d::North, Dir2d::West) => C { x: fx + 1., y: fy },
+            (Dir2d::South, Dir2d::East) => C { x: fx,      y: fy + 1. },
+            (Dir2d::South, Dir2d::West) => C { x: fx,      y: fy },
+            _ => panic!("bogus corner"),
+        }
+    } else {
+        match (s0.0, s1.0) {
+            (Dir2d::East, Dir2d::South) => C { x: fx + 1., y: fy },
+            (Dir2d::East, Dir2d::North) => C { x: fx,      y: fy },
+            (Dir2d::West, Dir2d::South) => C { x: fx + 1., y: fy + 1. },
+            (Dir2d::West, Dir2d::North) => C { x: fx,      y: fy + 1. },
+
+            (Dir2d::North, Dir2d::East) => C { x: fx,      y: fy },
+            (Dir2d::North, Dir2d::West) => C { x: fx,      y: fy + 1. },
+            (Dir2d::South, Dir2d::East) => C { x: fx + 1., y: fy },
+            (Dir2d::South, Dir2d::West) => C { x: fx + 1., y: fy + 1. },
+
+            _ => panic!("bogus corner"),
+        }
+    }
+}
+
+// This is going from a vector of trench-digging steps to a vector of points
+// which lie on one side of that trench; we arbitrarily call the sides side A
+// and side B, since we don't know which is which. Each point is formed by the
+// intersection of two lines, and we decide whether to outset a given part of
+// the coordinate pair (x or y) based on the directions of the two sides.
+fn perimeter_side(steps: &Vec<Step>, sideb: bool) -> LineString {
+    let mut result = Vec::new();
+    let mut p = Point2d { x: 0, y: 0 };
+
+    // The first corner is at (0,0) and its two edges are the last edge and the
+    // first edge.
+    result.push(corner(&p, &steps[steps.len() - 1], &steps[0], sideb));
+    for i in 0 .. steps.len() - 1 {
+        p = p.movebyn(steps[i].0, steps[i].1);
+        result.push(corner(&p, &steps[i], &steps[i + 1], sideb));
+    }
+
+    // Close the loop.
+    result.push(result[0]);
+
+    geo::LineString(result)
+}
+
+fn perimeter(steps: &Vec<Step>) -> LineString {
+    perimeter_side(steps, false)
+}
+
+fn parta(input: &str) -> usize {
+    let steps = parse(input, false);
+    let perim = perimeter(&steps);
+    let poly = Polygon::new(perim, Vec::new());
+    poly.unsigned_area() as usize
+}
+
+fn partb(input: &str) -> usize {
+    let steps = parse(input, true);
+    let perim = perimeter(&steps);
+    let poly = Polygon::new(perim, Vec::new());
+    poly.unsigned_area() as usize
+}
+
+pub fn solve(input: &str) -> (String, String) {
+    (parta(input).to_string(), partb(input).to_string())
 }
 
 #[test]
